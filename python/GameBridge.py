929ddd123a40f12e5546841b7933e75131ac38d1
dkoleber <dkedusp@gmail.com>
2019-11-13 17:59:10 -0700

    got most of IO working

--- python/GameBridge.py
+++ python/GameBridge.py
@@ -1,118 +1,316 @@
+from enum import Enum
+from queue import Queue
+from typing import List
+import threading
+from abc import ABC, abstractmethod
+
 import pyautogui as pg
 import time
+from IOAdapter import IOAdapter
 
 
-import ctypes
-import time
 
-SendInput = ctypes.windll.user32.SendInput
+global_io_adapter = IOAdapter()
 
-# C struct redefinitions
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+class IOAction(ABC):
+    def __init__(self, is_held):
+        self.is_held = is_held
 
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
+    @abstractmethod
+    def start(self):
+        pass
 
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+    @abstractmethod
+    def end(self):
+        pass
 
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
+    @abstractmethod
+    def extend(self):
+        pass
 
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
 
-# Actuals Functions
+class KeyAction(IOAction):
+    def __init__(self, is_held, key):
+        super().__init__(is_held)
+        self.key = key
 
-def PressKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+    def __eq__(self, other):
+        if isinstance(other, KeyAction):
+            return self.key == other.key and self.is_held == other.is_held
+        else:
+            return super().__eq__(other)
 
-def ReleaseKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+    def start(self):
+        print('staring key ' + self.key)
+        if not self.is_held:
+            global_io_adapter.key(self.key)
+        else:
+            global_io_adapter.key_down(self.key)
 
-# # directx scan codes http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
-# while (True):
-#     PressKey(0x11)
-#     time.sleep(1)
-#     ReleaseKey(0x11)
-#     time.sleep(1)
+    def end(self):
+        print('ending key ' + self.key)
+        if self.is_held:
+            global_io_adapter.key_up(self.key)
+
+    def extend(self):
+        print('extending key ' + self.key)
+        if not self.is_held:
+            global_io_adapter.key(self.key)
+
+
+class MouseAction(IOAction):
+    def __init__(self, is_held, key, x=None, y=None ):
+        super().__init__(is_held)
+        self.key = key
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if isinstance(other, MouseAction):
+            return self.key == other.key and self.x == other.x and self.y == other.y and self.is_held == other.is_held
+        else:
+            return super().__eq__(other)
+
+    def start(self):
+        if self.key == 'mouse_move' and self.x is not None and self.y is not None:
+            global_io_adapter.mouse_move_relative(self.x, self.y)
+        else:
+            if not self.is_held:
+                global_io_adapter.click()
+            else:
+                global_io_adapter.mouse_down()
+
+    def end(self):
+        if self.key == 'mouse_click' and self.is_held:
+            global_io_adapter.mouse_up()
+
+    def extend(self):
+        if self.key == 'mouse_click' and not self.is_held:
+            global_io_adapter.click()
+
+
+class IOActions:
+    def __init__(self, actions:List[IOAction]):
+        self.actions = actions
+
+    def start(self):
+        for action in self.actions:
+            action.start()
+
+    def end(self):
+        for action in self.actions:
+            action.end()
+
+    def extend(self):
+        for action in self.actions:
+            action.extend()
+
+    def __eq__(self, other):
+        if isinstance(other, IOActions):
+            if len(other.actions) != len(self.actions):
+                return False
+            for i in range(len(self.actions)):
+                if other.actions[i] != self.actions[i]:
+                    return False
+            return True
+        else:
+            return super().__eq__(other)
+
+
+class ActionType(Enum):
+    REAL = 0
+    '''
+    [0, .33) = action not taken
+    [.33, .66) = action taken
+    [.66, 1] = action held 
+    '''
+    PROBABILITY = 0
+
+
+class ActionDefinition:
+    def __init__(self, name, action_type):
+        self.name = name
+        self.action_type = action_type
+
+
+class ActionSpace:
+    def __init__(self, definition: List[ActionDefinition]):
+        self.definition = definition
+
+    def verify_action_vector(self, vector:List[float]):
+        if len(vector) != len(self.definition):
+            return False
+        # for i in range(len(vector)):
+        #     if self.definition[i].name != vector[i].name:
+        #         return False
+        #     if self.definition[i].action_type == ActionType.PROBABILITY and (vector[i] < 0 or vector[i] > 1):
+        #         return False
+        return True
+
+
+class ActionManager:
+    def __init__(self):
+        self.adapter = global_io_adapter
+        # self.queue = Queue()
+        self.space_definition = self.get_action_space_definition()
+        self.previous_actions = None
+
+    def execute_actions(self, action_vector:List[float]):
+        if self.space_definition.verify_action_vector(action_vector):
+            new_actions = self.io_actions_from_action_vector(action_vector)
+            if new_actions is not None:
+                if self.previous_actions == new_actions:
+                    self.previous_actions.extend()
+                else:
+                    if self.previous_actions is not None:
+                        self.previous_actions.end()
+                    new_actions.start()
+                    self.previous_actions = new_actions
+
+    def get_action_space_definition(self) -> ActionSpace:
+        action_names = ['mouse_click']
+        action_names.extend(self.adapter.get_available_keys())
+        result = [
+            ActionDefinition('mouse_x', ActionType.REAL),
+            ActionDefinition('mouse_y', ActionType.REAL),
+        ]
+        result.extend([ActionDefinition(name, ActionType.PROBABILITY) for name in action_names])
+        return ActionSpace(result)
+
+    def io_actions_from_action_vector(self, action_vector: List[float]) -> IOActions:
+        actions = [MouseAction(False, 'mouse_move', action_vector[0], action_vector[1])]
+        for i in range(2,3):
+            if action_vector[i] > .66:
+                actions.append(MouseAction(True, self.space_definition.definition[i].name))
+            elif action_vector[i] > .33:
+                actions.append(MouseAction(False, self.space_definition.definition[i].name))
+        for i in range(3, len(action_vector)):
+            if action_vector[i] > .66:
+                actions.append(KeyAction(True, self.space_definition.definition[i].name))
+            elif action_vector[i] > .33:
+                actions.append(KeyAction(False, self.space_definition.definition[i].name))
+
+        return IOActions(actions)
+
+class HardCodedAction:
+    def __init__(self, key, amount, delay, x=None, y=None):
+        self.key = key
+        self.amount = amount
+        self.delay = delay
+        self.x = x
+        self.y = y
+
+
+class BaseAgent(ABC):
+    def __init__(self, action_space: ActionSpace):
+        self.action_space = action_space
+
+    @abstractmethod
+    def get_action(self, agent_input):
+        pass
+
+class SequentialAgent(BaseAgent):
+    def __init__(self, action_space: ActionSpace):
+        super().__init__(action_space)
+
+        self.key_translator = {}
+        for i in range(len(self.action_space.definition)):
+            self.key_translator[self.action_space.definition[i].name] = i
+
+        self.movement_sequence = [
+            # HardCodedAction('1', .4, 3),
+            # HardCodedAction('2', .4, 3),
+            # HardCodedAction('a', .7, 3),
+            # HardCodedAction('a', .7, 3),
+            # HardCodedAction('a', .4, 3),
+            # HardCodedAction('a', .0,  1),
+            # HardCodedAction('d', .4, 3),
+            # HardCodedAction('d', .0, 1),
+            # HardCodedAction('space', .4,  1),
+            HardCodedAction('a', .0,  1, 100, 100),
+        ]
+
+
+    def get_action(self, agent_input) -> List[float]:
+        result = [0. for x in range(len(self.action_space.definition))]
+        if len(self.movement_sequence) > 0:
+            movement = self.movement_sequence[0]
+
+            result[self.key_translator[movement.key]] = movement.amount
+            if movement.x is not None and movement.y is not None:
+                result[self.key_translator['mouse_x']] = movement.x
+                result[self.key_translator['mouse_y']] = movement.y
+
+            time.sleep(movement.delay)
+            del self.movement_sequence[0]
+
+        return result
 
 
 def run():
-    key_1 = 0x02
-    key_2 = 0x03
-
-    key_a = 0x1E
-
-
-    middle_x = int(1920 / 2)
-    middle_y = int(1080 / 2)
-
     time.sleep(5)
+    manager = ActionManager()
+    action_space = manager.get_action_space_definition()
+    agent = SequentialAgent(action_space)
 
-    print('staring keys 1')
+    while(True):
+        agent_input = global_io_adapter.get_screen()
+        agent_output = agent.get_action(agent_input)
+        manager.execute_actions(agent_output)
 
-    PressKey(key_1)
-    time.sleep(.1)
-    ReleaseKey(key_1)
-    time.sleep(1)
 
-    PressKey(key_2)
-    time.sleep(.1)
-    ReleaseKey(key_2)
-    time.sleep(1)
 
-    PressKey(key_a)
-    time.sleep(3)
-    ReleaseKey(key_a)
-    time.sleep(1)
-
-    print('starting keys')
-
-    pg.press('1')
-    time.sleep(1)
-    pg.press('2')
-    time.sleep(1)
-    pg.press('3')
-    time.sleep(3)
-
-    pg.keyDown('w')
-    time.sleep(3)
-    pg.keyUp('w')
-    time.sleep(1)
-
-    print('starting mouse')
-
-    pg.moveTo(middle_x, middle_y, 1)
-    time.sleep(1)
-    pg.moveTo(middle_x, middle_y + 50, 1)
-    pg.mouseDown()
-    time.sleep(5)
-    pg.mouseUp()
+# def run():
+#     key_1 = 0x02
+#     key_2 = 0x03
+#
+#     key_a = 0x1E
+#
+#
+#     middle_x = int(1920 / 2)
+#     middle_y = int(1080 / 2)
+#
+#     time.sleep(5)
+#
+#     print('staring keys 1')
+#
+#     PressKey(key_1)
+#     time.sleep(.1)
+#     ReleaseKey(key_1)
+#     time.sleep(1)
+#
+#     PressKey(key_2)
+#     time.sleep(.1)
+#     ReleaseKey(key_2)
+#     time.sleep(1)
+#
+#     PressKey(key_a)
+#     time.sleep(3)
+#     ReleaseKey(key_a)
+#     time.sleep(1)
+#
+#     print('starting keys')
+#
+#     pg.press('1')
+#     time.sleep(1)
+#     pg.press('2')
+#     time.sleep(1)
+#     pg.press('3')
+#     time.sleep(3)
+#
+#     pg.keyDown('w')
+#     time.sleep(3)
+#     pg.keyUp('w')
+#     time.sleep(1)
+#
+#     print('starting mouse')
+#
+#     pg.moveTo(middle_x, middle_y, 1)
+#     time.sleep(1)
+#     pg.moveTo(middle_x, middle_y + 50, 1)
+#     pg.mouseDown()
+#     time.sleep(5)
+#     pg.mouseUp()
 
 if __name__=='__main__':
     run()
---
+++ python/IOAdapter.py
@@ -0,0 +1,162 @@
+import ctypes
+import time
+import pyautogui as pg
+import mss
+import numpy as np
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+
+adapter_dict_1 = {
+    'a': 0x1C,
+    'b': 0x32, #buff
+    'd': 0x23,
+    'e': 0x24,
+    'h': 0x33, #heal
+    'j': 0x3B, #mana
+    'r': 0x2D, #mount
+    's': 0x1B,
+    't': 0x2C, #throw
+    'w': 0x1D,
+    '1': 0x16,
+    '2': 0x1E,
+    '3': 0x26,
+    '4': 0x25,
+    '5': 0x2E,
+    '6': 0x36,
+    '7': 0x3D,
+    '8': 0x3E,
+    '9': 0x46,
+    '0': 0x45,
+    'esc': 0x76,
+    'enter': 0x5A,
+    'lctrl': 0x14,
+    'lshift': 0x12,
+    'space': 0x29,
+    'default': 0x00,
+}
+
+adapter_dict_2 = {
+    'a': 0x1E,
+    'b': 0x30,  # buff
+    'd': 0x20,
+    'e': 0x12,
+    'h': 0x23,  # heal
+    'j': 0x24,  # mana
+    'r': 0x13,  # mount
+    's': 0x1F,
+    't': 0x14,  # throw
+    'w': 0x11,
+    '1': 0x02,
+    '2': 0x03,
+    '3': 0x04,
+    '4': 0x05,
+    '5': 0x06,
+    '6': 0x07,
+    '7': 0x08,
+    '8': 0x09,
+    '9': 0x0A,
+    '0': 0x0B,
+    'esc': 0x01,
+    'enter': 0x1C,
+    'lctrl': 0x1D, #maybe applicable to only non-us keyboards
+    'lshift': 0x2A, #maybe applicable to only non-us keyboards
+    'space': 0x39,
+    'default': 0x00,
+}
+
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+class IOAdapter:
+    def __init__(self):
+        self.adapter_dict = adapter_dict_2
+
+        self.capture_device = mss.mss()
+        self.monitor_dim = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080} #TODO: do this dynamically per monitor
+        self.center_x = self.monitor_dim['left'] + int(self.monitor_dim['width'] / 2)
+        self.center_y = self.monitor_dim['top'] + int(self.monitor_dim['height'] / 2)
+
+    def get_available_keys(self):
+        return list(self.adapter_dict.keys())
+
+    def key(self, key, duration=.1):
+        # for efficiency, doesn't call into other class functions since they also do checks for key existence
+        if key in self.adapter_dict:
+            PressKey(self.adapter_dict[key])
+            time.sleep(duration)
+            ReleaseKey(self.adapter_dict[key])
+
+    def key_down(self, key):
+        if key in self.adapter_dict:
+            print('pressing key ' + key)
+            PressKey(self.adapter_dict[key])
+
+    def key_up(self, key):
+        if key in self.adapter_dict:
+            print('releasing key ' + key)
+            ReleaseKey(self.adapter_dict[key])
+
+    def mouse_move_relative(self, x, y, duration=None):
+        print('moving mouse')
+        if duration is None:
+            pg.moveTo(x + self.center_x, y + self.center_y)
+        else:
+            pg.moveTo(x + self.center_x, y + self.center_y, duration)
+
+    def mouse_down(self):
+        pg.mouseDown()
+
+    def mouse_up(self):
+        pg.mouseUp()
+
+    def click(self):
+        pg.click()
+
+    def get_screen(self):
+        return np.array(self.capture_device.shot())
